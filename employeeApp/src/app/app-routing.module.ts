4f754dbd9d41b1d76fac4cf731e8f0d31b1bea8c
Sharath chandra <Sharath.chandrag2@gmail.com>
2020-12-26 21:17:33 +0530

    Updated Employee app

--- employeeApp/src/app/app-routing.module.ts
+++ employeeApp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { DeleteEmployeeComponent } from './components/delete-employee/delete-employee.component';
+import { EmpListComponent } from './components/emp-list/emp-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: EmpListComponent},
+  {path: 'create', component: CreateEmployeeComponent},
+  {path: 'delete', component: DeleteEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- employeeApp/src/app/app.module.ts
+++ employeeApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { DeleteEmployeeComponent } from './components/delete-employee/delete-employee.component';
+import { EmpListComponent } from './components/emp-list/emp-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    DeleteEmployeeComponent,
+    EmpListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
